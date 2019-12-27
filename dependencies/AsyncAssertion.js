48bc483a4083ea89f8179a50378c384dff58a542
Chris Stead <cmstead@gmail.com>
2019-12-27 15:52:23 -0800

    [Feature] Added equality check and proper chaining

--- dependencies/AsyncAssertion.js
+++ dependencies/AsyncAssertion.js
@@ -1,16 +1,29 @@
 const { assert } = require('chai');
 
-function Assertion() {}
-
-Assertion.prototype = {
-    equals: function () {}
-};
-
-function AsyncAssertion() {}
+function AsyncAssertion(asyncAction) {
+    this.asyncAction = asyncAction;
+    this.resultTransform = null;
+}
 
 AsyncAssertion.prototype = {
-    assertResult: function() {
-        return new Assertion();
+    assertResult: function(resultTransform) {
+        // if(this.resultTransform !== null) {
+        //     throw new Error('Function assertResult cannot be called more than once.');
+        // }
+
+        this.resultTransform = resultTransform;
+
+        return this;
+    },
+
+    equals: function (expectedResult, message) {
+        return new Promise((resolve, reject) => {
+            this.asyncAction()
+                .then(this.resultTransform)
+                .then((value) => assert.equal(value, expectedResult, message))
+                .then(() => resolve(true))
+                .catch((error) => reject(error));
+        });
     }
 };
 
--- tests/assertion.test.js
+++ tests/assertion.test.js
@@ -1,5 +1,10 @@
 const AsyncAssertion = require('../dependencies/AsyncAssertion');
 const promisePuppeteer = require('promise-puppeteer');
+const { assert } = require('chai');
+
+function delayedResolve(thenableFake, result) {
+    setTimeout(() => thenableFake.resolve(result), 15);
+}
 
 describe("Assertion", function () {
 
@@ -16,11 +21,33 @@ describe("Assertion", function () {
     it("uses a transform to capture result for assertion", function () {
         const actualResult = { message: 'YAY' };
 
-        assertion
+        delayedResolve(thenableFake, actualResult);
+
+        return assertion
             .assertResult(result => result.message)
             .equals('YAY');
+    });
 
-        thenableFake.resolve(actualResult);
+    it("fails when transform result doesn't match expected result", function () {
+        const actualResult = { message: 'bad message' };
+
+        delayedResolve(thenableFake, actualResult);
+
+        let resolutionState = true;
+
+        return assertion
+
+            .assertResult(result => result.message)
+            .equals('ANOTHER YAY')
+
+            .then(() => {
+                resolutionState = false;
+
+                assert.isTrue(resolutionState);
+            })
+            .catch(() => {
+                assert.isTrue(resolutionState);
+            });
     });
 
 });
