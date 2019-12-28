c7c8f5b8222bf0eae72b40c8a127a6402b66bc44
Chris Stead <cmstead@gmail.com>
2019-12-27 16:55:12 -0800

    [Feature] Added error verification

--- dependencies/AsyncAssertion.js
+++ dependencies/AsyncAssertion.js
@@ -4,27 +4,75 @@ const AsyncActionResolver = require('./AsyncActionResolver');
 function AsyncAssertion(asyncAction) {
     this.asyncActionResolver = new AsyncActionResolver(asyncAction);
     this.resultTransform = null;
+    this.errorTransform = null;
 }
 
 AsyncAssertion.prototype = {
-    assertResult: function(resultTransform) {
-        if(this.resultTransform !== null) {
-            throw new Error('Function assertResult cannot be called more than once.');
+    throwOnDuplicateCall: function () {
+        if (this.resultTransform !== null || this.errorTransform !== null) {
+            const message = 'Functions assertResult ' +
+                'and assertError cannot be used together, ' +
+                'or called more than once.'
+            throw new Error(message);
         }
+    },
+    assertResult: function (resultTransform) {
+        this.throwOnDuplicateCall();
 
         this.resultTransform = resultTransform;
 
         return this;
     },
 
-    equals: function (expectedResult, message) {
+    assertError: function (resultTransform) {
+        this.throwOnDuplicateCall()
+
+        this.errorTransform = resultTransform;
+
+        return this;
+    },
+
+    buildResolutionHandler: function (resolve, reject, args) {
+        return (...results) => {
+            if (typeof this.resultTransform === 'function') {
+                const actualResult = this.resultTransform(...results);
+                assert.equal(...[actualResult].concat(args));
+
+                resolve(true);
+            } else {
+                reject(new Error('[RequestAssert] Expected an error, but got a success result.'));
+            }
+        }
+    },
+
+    buildRejectionHandler: function (resolve, reject, args) {
+        return (...results) => {
+            if (typeof this.errorTransform === 'function') {
+                try {
+                    const actualResult = this.errorTransform(...results);
+
+                    assert.equal(...[actualResult].concat(args));
+
+                    resolve(true);
+                } catch (e) {
+                    reject(e);
+                }
+            } else {
+                reject(new Error('[RequestAssert] Expected a success result, but got an error.'));
+            }
+        }
+    },
+
+    equals: function (...args) {
         return new Promise((resolve, reject) => {
+            const handleResolution = this.buildResolutionHandler(resolve, reject, args);
+            const handleError = this.buildRejectionHandler(resolve, reject, args)
+
+
             this.asyncActionResolver
                 .resolve()
-                .then(this.resultTransform)
-                .then((value) => assert.equal(value, expectedResult, message))
-                .then(() => resolve(true))
-                .catch((error) => reject(error));
+                .then(handleResolution)
+                .catch(handleError);
         });
     }
 };
--- tests/assertion.test.js
+++ tests/assertion.test.js
@@ -6,6 +6,10 @@ function delayedResolve(thenableFake, result) {
     setTimeout(() => thenableFake.resolve(result), 15);
 }
 
+function delayedReject(thenableFake, result) {
+    setTimeout(() => thenableFake.reject(result), 15);
+}
+
 describe("Assertion", function () {
 
     let assertion;
@@ -18,52 +22,69 @@ describe("Assertion", function () {
         assertion = new AsyncAssertion(asyncAction);
     });
 
-    it("uses a transform to capture result for assertion", function () {
-        const actualResult = { message: 'YAY' };
+    describe("assertResult", function () {
+        it("uses a transform to capture result for assertion", function () {
+            const actualResult = { message: 'YAY' };
 
-        delayedResolve(thenableFake, actualResult);
+            delayedResolve(thenableFake, actualResult);
 
-        return assertion
-            .assertResult(result => result.message)
-            .equals('YAY');
+            return assertion
+                .assertResult(result => result.message)
+                .equals('YAY');
+        });
+
+        it("fails when transform result doesn't match expected result", function () {
+            const actualResult = { message: 'bad message' };
+
+            delayedResolve(thenableFake, actualResult);
+
+            let resolutionState = true;
+
+            return assertion
+
+                .assertResult(result => result.message)
+                .equals('ANOTHER YAY')
+
+                .then(() => {
+                    resolutionState = false;
+
+                    assert.isTrue(resolutionState, 'Assertion did not fail on bad equality');
+                })
+                .catch(() => {
+                    assert.isTrue(resolutionState);
+                });
+        });
+
+        it("throws an error when assertResult is called more than once", function () {
+            assert.throws(
+                () => assertion.assertResult(() => null).assertResult(),
+                'Functions assertResult and assertError cannot be used together, or called more than once.');
+        });
+
+        it("properly resolves callback-style async actions", function () {
+            function callbackStyleAction(callback) {
+                setTimeout(() => callback(null, 'something'), 15);
+            }
+
+            const assertion = new AsyncAssertion(callbackStyleAction);
+
+            return assertion
+                .assertResult(x => x)
+                .equals('something');
+        });
     });
 
-    it("fails when transform result doesn't match expected result", function () {
-        const actualResult = { message: 'bad message' };
+    describe("assertError", function(){
+        it("uses a transform to capture error for assertion", function () {
+            const actualError = new Error('Something broke');
 
-        delayedResolve(thenableFake, actualResult);
+            delayedReject(thenableFake, actualError);
 
-        let resolutionState = true;
+            return assertion
+                .assertError(result => result.message)
+                .equals('Something broke');
+        });
 
-        return assertion
 
-            .assertResult(result => result.message)
-            .equals('ANOTHER YAY')
-
-            .then(() => {
-                resolutionState = false;
-
-                assert.isTrue(resolutionState, 'Assertion did not fail on bad equality');
-            })
-            .catch(() => {
-                assert.isTrue(resolutionState);
-            });
     });
-
-    it("throws an error when assertResult is called more than once", function(){
-        assert.throws(() => assertion.assertResult(() => null).assertResult(), 'Function assertResult cannot be called more than once.');
-    });
-
-    it("properly resolves callback-style async actions", function(){
-        function callbackStyleAction (callback) {
-            setTimeout(() => callback(null, 'something'), 15);
-        }
-
-        const assertion = new AsyncAssertion(callbackStyleAction);
-
-        return assertion
-            .assertResult(x => x)
-            .equals('something');
-    });
-
 });
